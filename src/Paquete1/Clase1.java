a0bd3e1ebf33633a18500adaa72c5140376c010c
javigis1 <javigismart@hotmail.com>
2026-02-10 23:27:12 +0100

    archivos movidos a paquete

--- src/Paquete1/Clase1.java
+++ src/Paquete1/Clase1.java
@@ -1,14 +1,16 @@
+package Paquete1;
+
 public class Clase1 {
 
     /*JFrame frame;
 
-        Clase1() {
+        Paquete1.Clase1() {
             frame = new JFrame();
             JOptionPane.showMessageDialog(frame, "Hola, bienvenido a este tutorial de JOptionPane.");
         }
 
         public static void main(String[] args) {
-            new Clase1();
+            new Paquete1.Clase1();
         }
     }*/
 }
