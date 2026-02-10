25c081864f69e0b827cbde095e04594c28f2fe20
javigis1 <javigismart@hotmail.com>
2026-02-10 22:59:32 +0100

    Repositorio creado

---
+++ src/Clase1.java
@@ -0,0 +1,14 @@
+public class Clase1 {
+
+    /*JFrame frame;
+
+        Clase1() {
+            frame = new JFrame();
+            JOptionPane.showMessageDialog(frame, "Hola, bienvenido a este tutorial de JOptionPane.");
+        }
+
+        public static void main(String[] args) {
+            new Clase1();
+        }
+    }*/
+}
