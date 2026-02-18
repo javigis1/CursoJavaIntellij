608973d1def5d41a76aef58f70945e5577c30d02
javigis1 <javigismart@hotmail.com>
2026-02-18 23:32:07 +0100

    Arreglado el main

--- src/Paquete1/Main.java
+++ src/Paquete1/Main.java
@@ -1,21 +1,23 @@
-public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
-    System.out.print("Hola, hago un cambio desde github mientras lo hacia tambien en local!");
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        System.out.println("i = " + i);
-    }
-int [][] matriz = new int[4][5] ;
-    for (int i =0; i <4; i++) {
-        for (int j = 0; j < 5; j++) {
-            matriz[i][j] = i + j;
+        System.out.print("Hola, hago un cambio desde github mientras lo hacia tambien en local!");
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-    }
-    for (int i =0; i <4; i++) {
-        for (int j = 0; j < 5; j++) {
-            System.out.println(matriz [i][j]);
+        int[][] matriz = new int[4][5];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = i + j;
+            }
         }
-    }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
 
+    }
 }
