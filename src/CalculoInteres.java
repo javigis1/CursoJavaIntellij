b788cb7090e64b89a01a0730a16a7ed15c5652c3
javigis1 <javigismart@hotmail.com>
2026-02-20 23:53:02 +0100

    Calculo interes

--- src/CalculoInteres.java
+++ src/CalculoInteres.java
@@ -1,4 +1,31 @@
-public class CalculoInteres {
+import javax.swing.*;
 
+public class CalculoInteres {
+    public static void main  (String[] args)  {
+
+        double acumulado= 0;
+        double interes=0;
+        double valorinicial=0;
+        int numeroInt=0;
+        double[][] matriz = new double[4][5];
+        for (int i = 0; i < 2; i++) {
+
+            //JOptionPane.showMessageDialog(frame, "Hola, introduce valor inicial.");
+            valorinicial= Integer.parseInt(JOptionPane.showInputDialog("Introduce valor inicial "));
+            interes= Integer.parseInt(JOptionPane.showInputDialog("Introduce interes "));
+
+            acumulado= acumulado+(valorinicial*interes)/100;
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = acumulado ;
+            }
+
+
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
+    }
 
 }
