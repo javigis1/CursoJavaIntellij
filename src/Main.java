cb0145498c8335809c4e9abff9724802506b54ac
javigis1 <javigismart@hotmail.com>
2026-02-20 23:07:00 +0100

    Amplio main

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,16 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        int[][] matriz = new int[4][5];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = i + j;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
     }
 }
